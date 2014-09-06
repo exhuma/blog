a6ea215c4ef65666564ffb47ad5522990829b7b0
Michel Albert <michel@albert.lu>
2014-09-06 20:28:07 +0200

    Removed test-themes from config.

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,30 +24,6 @@ LINKS = (
 )
 
 THEME = 'theme'
-#THEME = '/home/exhuma/work/__libs__/pelican-themes/chunk'
-#THEME = '/home/exhuma/work/__libs__/pelican-themes/notmyidea-cms'
-#THEME = '/home/exhuma/work/__libs__/pelican-themes/pelican-mockingbird'
-#THEME = '/home/exhuma/work/__libs__/pelican-themes/pelican-simplegrey'
-#THEME = '/home/exhuma/work/__libs__/pelican-themes/pelican-sober'
-#THEME = '/home/exhuma/work/__libs__/pelican-themes/sundown'
-#THEME = '/home/exhuma/work/__libs__/pelican-themes/zurb-F5-basic'
-
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/BT3-Flat
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/Just-Read'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/README.rst'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/Responsive-Pelican'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/SoMA'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/SoMA2'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/aboutwilson'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/basic'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/bluegrasshopper'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/blueidea'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/bold'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/bootstrap'
-#broken? THEME = '/home/exhuma/work/__libs__/pelican-themes/bootstrap2'
-#assets THEME = '/home/exhuma/work/__libs__/pelican-themes/burrito'
-#articles_page THEME = '/home/exhuma/work/__libs__/pelican-themes/lazystrap'
-#assets THEME = '/home/exhuma/work/__libs__/pelican-themes/sora'
 
 # Social widget
 SOCIAL = (
